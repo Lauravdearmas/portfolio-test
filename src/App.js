1a04f4a5f6f9d4e9438c22b7cb1902d95e41282c
laura <loridarmas@gmail.com>
2021-07-23 18:35:05 -0500

    build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Content } from "./components/Content";
 import { BodyPortfolio } from "./components/Portfolio";
-import { AnimateElements } from "./components/Pizza";
-import { AnimateIdeaElements } from "./components/idea";
-import { AnimateMatrazElements } from "./components/matraz";
 import { Toggle } from "./components/Toggle";
 import { useDarkMode } from "./styles/useDarkMode";
 import { GlobalStyles, lightTheme, darkTheme } from "./styles/globalStyles";
@@ -25,10 +22,6 @@ function App() {
         <Toggle theme={theme} toggleTheme={toggleTheme} />
         <Content />
         <BodyPortfolio />
-        {/* <AnimateElements />
-
-        <AnimateIdeaElements /> */}
-        {/* <AnimateMatrazElements />  */}
       </Container>
     </ThemeProvider>
   );
